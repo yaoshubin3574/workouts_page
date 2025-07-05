0e5a2d51d7b336a300d17ca6d4a168561a07eef4
yaoshubin3574 <33371737+yaoshubin3574@users.noreply.github.com>
2025-07-06 02:27:45 +0800

    Update const.ts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -53,8 +53,9 @@ const RICH_TITLE = true;
 const IS_CHINESE = true;
 const USE_ANIMATION_FOR_GRID = false;
 const CHINESE_INFO_MESSAGE = (yearLength: number, year: string): string =>
-  `我记录自己运动 ${yearLength} 年了 ` + ( year === 'Total' ? '' : `，下面列表展示的是 ${year} 年的数据`);
-
+   const yearStr = year === 'Total' ? '所有' : ` ${year} `;
+  return `Le vent se lève, il faut tenter de vivre.`;
+};
 const ENGLISH_INFO_MESSAGE = (yearLength: number, year: string): string =>
   `Logged ${yearLength} Years of Outdoor Journey` +  ( year === 'Total' ? '' : `, the map show routes in ${year}`);
 
