1581760f42822191c3b9aa38235642aa5da831f1
yaoshubin3574 <33371737+yaoshubin3574@users.noreply.github.com>
2025-10-24 11:02:06 +0800

    Change default values for special distances

--- run_page/gen_svg.py
+++ run_page/gen_svg.py
@@ -148,7 +148,7 @@ def main():
         dest="special_distance",
         metavar="DISTANCE",
         type=float,
-        default=20.0,
+        default=10.0,
         help="Special Distance1 by km and color with the special_color",
     )
     args_parser.add_argument(
@@ -156,7 +156,7 @@ def main():
         dest="special_distance2",
         metavar="DISTANCE",
         type=float,
-        default=40.0,
+        default=20.0,
         help="Special Distance2 by km and corlor with the special_color2",
     )
     args_parser.add_argument(
